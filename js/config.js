bf3b12edd282ce1fa2d899b30970927c5b1bc169
Jokerkingsss <wubaichuan123@gmail.com>
2025-05-18 20:41:09 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -109,6 +109,9 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
+    hanime: {
+        api:'https://hanime1.me',
+        name: 'H漫资源'
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
